046f1fecb7194c610d692bf9c69b1b2e6247bb2f
saite <saitejasai57@gmail.com>
2023-12-14 21:41:31 +0530

    new

--- 02-variables.sh
+++ 02-variables.sh
@@ -8,4 +8,6 @@ echo "$Person1 loves $Person2 batting"
 
 echo "Please enter the UserName"
 read USERNAME
+read -s PASSWORD
 echo "Entered UserName is: $USERNAME"
+echo "Entered Password is: $PASSWORD"
