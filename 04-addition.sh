828279710eb5cd7c54a20f84657a8ece514ff3ea
saite <saitejasai57@gmail.com>
2023-12-14 21:51:29 +0530

    new commit

--- 04-addition.sh
+++ 04-addition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -m "Enter Number1" NUMBER1
-read -m"Entere Number2" NUMBER2
+read -p "Enter Number1" NUMBER1
+read -p "Entere Number2" NUMBER2
 
 SUM=$(($NUMBER1+$NUMBER2))
 echo "SUM is: $SUM"
