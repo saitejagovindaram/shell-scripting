5a87ba6afa44da14923674188319c58932cc07d1
saite <saitejasai57@gmail.com>
2023-12-14 22:09:57 +0530

    new commit

--- 03-shellPractice.sh
+++ 03-shellPractice.sh
@@ -2,13 +2,13 @@
 
 DATE=$(date)
 echo "script started executing at: $DATE"
-echo "################################################"
+echo "################### ARGUMENTS #############################"
 echo "argument 0, i.e \$0 is fileName: $0"
 echo "argument 1, i.e \$1: $1"
 echo "argument 2, i.e \$2: $2"
 echo "list of arguments passed are, \$@: $@"
 echo "Number of arguments passed, \$#: $#"
-echo "################################################"
+echo "################### ARRAYS #############################"
 
 FRUITS=("banana", "apple", "pineapple", "strawberry")
 echo "first fruit: ${FRUITS[0]}"
@@ -16,3 +16,11 @@ echo "second fruit: ${FRUITS[1]}"
 echo "third fruit: ${FRUITS[2]}"
 echo "fourth fruit: ${FRUITS[3]}"
 echo "all fruits: ${FRUITS[@]}"
+
+echo "################################################"
+
+STR1="Saiteja"
+STR2="Govindaram"
+echo "\$STR1+\$STR2: $STR1+$STR2"
+echo "\$STR1\$STR2": $STR1$STR2
+echo "\$STR1 \$STR2: $STR1 $STR2
