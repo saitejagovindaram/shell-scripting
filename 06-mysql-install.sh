c6d300e95be0be29f5d890b2b320776855c3257c
saite <saitejasai57@gmail.com>
2023-12-14 23:51:49 +0530

    new

--- 06-mysql-install.sh
+++ 06-mysql-install.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then    
-    echo "Not permitted to run, please run as root user"
+    echo "Not Allowed to run, please run as root user"
     exit 1  #exit status 0 is success, we can it exit status through $?
 fi
 
