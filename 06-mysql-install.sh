9e3cd865f65359b0b93952fbfaf4db576862d065
saite <saitejasai57@gmail.com>
2023-12-15 16:22:49 +0530

    new

--- 06-mysql-install.sh
+++ 06-mysql-install.sh
@@ -14,15 +14,15 @@ N="\e[0m"
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $R $2 $N"
+        echo -e "ERROR:: $R $2 $N"
     else
-        echo "SUCCESS:: $G $2 $N"
+        echo -e "SUCCESS:: $G $2 $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then    
-    echo "$R Not Allowed to run, please run as root user $N"
+    echo -e "$R Not Allowed to run, please run as root user $N"
     exit 1  #exit status 0 is success, we can this exit status through $?
 fi
 
