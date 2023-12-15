0d2f6a16eec1a622feb6f06cb120620f634b369e
saite <saitejasai57@gmail.com>
2023-12-15 15:23:55 +0530

    new

--- 06-mysql-install.sh
+++ 06-mysql-install.sh
@@ -14,7 +14,7 @@ validate(){
 if [ $ID -ne 0 ]
 then    
     echo "Not Allowed to run, please run as root user"
-    exit 1  #exit status 0 is success, we can it exit status through $?
+    exit 1  #exit status 0 is success, we can this exit status through $?
 fi
 
 yum install mysql -y
