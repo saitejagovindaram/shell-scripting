798eaba56fb351ac2ef0f2da8d35807cda50e3f0
saite <saitejasai57@gmail.com>
2023-12-18 22:44:08 +0530

    new

---
+++ 08-install-package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ ID -ne 0 ]
+then
+    echo "You dont have permission, Please run using Root access"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ... FAILED"
+    else
+        echo "$1 ... SUCCESS"
+}
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+
+    else
+        echo "$package is already installed ... Skipped"
+    fi
+done
