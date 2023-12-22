5b5ab7b3e3b03d87c3ee53f4a538b66418f16e61
saite <saitejasai57@gmail.com>
2023-12-22 12:19:34 +0530

    new

--- 09-mysql-setup.sh
+++ 09-mysql-setup.sh
@@ -2,8 +2,14 @@
 
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $ID -ne 0 ]
 then 
-    echo "You are not a root user"
+    echo -e "$R You are not a root user $N"
     exit 2
 fi
+
