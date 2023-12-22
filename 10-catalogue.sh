7c002784e035f4e9f14d3239f7057e0eaf1f04f6
saite <saitejasai57@gmail.com>
2023-12-22 13:17:21 +0530

    new

---
+++ 10-catalogue.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%f-%T)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILED $N"
+        exit 2
+    else
+        echo -e "$2 .... $G SUCCESS $N"
+    fi
+}
+
+checkroot(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$R You are not a root user $N"
+        exit 2
+    fi
+}
+
+ID=$(id -u)
+checkroot $?
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "Enabling nodejs 18"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing nodejs 18"
+
+useradd roboshop &>> $LOGFILE
+VALIDATE $? "Adding roboshop user for app"
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "downloading catalogue appliation"
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "unzipping catalogue"
+
+cd /app
+npm install &>> $LOGFILE
+VALIDATE $? "Installing npm package manager"
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "copying catalgue service unit file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Reload deemon"
+
+systemctl enable catalogue
+VALIDATE $? "enabling catalogue"
+
+systemctl start catalogue
+VALIDATE $? "starting catalogue"
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copying mongo repo"
+
+dnf install mongodb-org-shell -y
+VALIDATE $? "Installing mongo client"
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE $? "Loading data to mongodb"
+
