367a71c7d6055375d95f21cdc72741150d5e555f
saite <saitejasai57@gmail.com>
2023-12-22 23:31:24 +0530

    new

--- 10-catalogue.sh
+++ 10-catalogue.sh
@@ -78,6 +78,6 @@ VALIDATE $? "copying mongo repo"
 dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "Installing mongo client"
 
-mongo --host 172.31.87.193 </app/schema/catalogue.js &>> $LOGFILE
+mongo --host mongo.saitejag.site </app/schema/catalogue.js &>> $LOGFILE
 VALIDATE $? "Loading data to mongodb"
 
