9646a600b6aef53f517b465b680f587b1a310cd0
saite <saitejasai57@gmail.com>
2023-12-23 22:11:32 +0530

    new

--- 21-delete-old-loggiles.sh
+++ 21-delete-old-loggiles.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-sourceDir=/tmp/shell-logs/
+sourceDir1=/tmp/shell-logs/
+sourceDir2=/tmp/shell-files/
 
-filesToDelete=$(find $sourceDir -type f -name "*.log")
+filesToDelete1=$(find $sourceDir1 -type f -name "*.log")
+filesToDelete2=$(find $sourceDir2 -type f -name "*.sh")
 
+#using for loop
 for file in $filesToDelete
 do  
     echo "Deleting file: $file"
 done
+
+#using while loop
+while file
+do 
+    echo "Deleting file: $file"
+done < $filesToDelete2
