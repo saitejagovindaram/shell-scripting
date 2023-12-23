b6cea183a729df7f45a0b2b7c2e99ef74ccf0d32
saite <saitejasai57@gmail.com>
2023-12-23 21:57:55 +0530

    new

--- 21-advance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-type pwd
-type -a ls
-type -p ls
---
+++ 21-delete-old-loggiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sourceDir=/tmp/shell-logs
+
+filesToDelete=find $sourceDir -type f -name "*.log"
+
+for file in $filesToDelete
+do  
+    echo "Deleting file: $file"
+done
