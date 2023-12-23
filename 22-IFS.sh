12018156a186f9c14447e6043ef75c27b6a4e842
saite <saitejasai57@gmail.com>
2023-12-23 22:53:46 +0530

    new

--- 22-IFS.sh
+++ 22-IFS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-find "/tmp/shell-files" -type f -mtime +14 -name "*.sh" -exec rm {} \;
+file=/etc/passwd
 
-# find "$sourceDir2": Searches in the specified directory ($sourceDir2).
-# -type f: Looks for files (not directories or other types of files).
-# -mtime +14: Filters files modified more than 14 days ago.
-# -name "*.sh": Matches files with names ending in .sh.
-# -exec rm {} \;: Executes the rm command on each file found by find. {} represents the found file, and \; indicates the end of the -exec command.
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do
+    echo "username: $username"
+    echo "user ID: $user_id"
+    echo "User Full name: $user_fullname"
+done < $file
 
