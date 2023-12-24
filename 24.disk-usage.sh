9245b32c0a1c53a45a2e2670b1a71b7432536a1c
saite <saitejasai57@gmail.com>
2023-12-24 10:56:47 +0530

    new

--- 24.disk-usage.sh
+++ 24.disk-usage.sh
@@ -4,8 +4,8 @@ volumesData=$( df -hT | grep -vE 'tmp|File')
 
 while IFS= read line
 do
-    diskName=$(echo $line | awk -F '{print $1F}')
-    usage=$(echo $line | awk -F '{print $6F}')
+    diskName=$(echo $line | awk -F ' ' '{print $1F}')
+    usage=$(echo $line | awk -F ' ' '{print $6F}')
     echo $diskName
     echo $usage
 done <<< "$volumesData"
