e15c8d9cabed0cc4fa4cd206bb4bb6d560c8d830
saite <saitejasai57@gmail.com>
2023-12-24 11:11:27 +0530

    new

--- 22-IFS.sh
+++ 22-IFS.sh
@@ -2,6 +2,11 @@
 
 file=/etc/passwd
 
+if [ ! -f $file ] # ! denotes opposite
+then
+    echo -e "Source directory: $file does not exists."
+fi
+
 while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
     echo "username: $username"
