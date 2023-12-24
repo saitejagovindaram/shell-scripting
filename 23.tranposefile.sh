025e28295e4964bec642dc77daca38f0c0aff655
saite <saitejasai57@gmail.com>
2023-12-24 12:06:53 +0530

    new

--- 23.tranposefile.sh
+++ 23.tranposefile.sh
@@ -2,6 +2,7 @@
 
 file=tranpose.txt
 
+#approach 1, not suitable for large dataset
 col1=$(awk -F ' '  '{print $1F}' transpose.txt)
 col2=$(awk -F ' '  '{print $2F}' transpose.txt)
 
@@ -14,3 +15,16 @@ for line in $col2
 do
     echo -n "$line  " 
 done
+
+###############
+cat trasnpose.txt | wc -l # gives no of rows
+cat transpose.txt | head -n 1 | awk '{print NF}' # gives no of columns
+
+NoOfCols=$(cat transpose.txt | head -n 1 | awk '{print NF}')
+
+for col in NoOfCols
+do
+    echo $col
+done
+
+
