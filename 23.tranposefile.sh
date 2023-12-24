3eda2e78622c9454f12831c2e191aab5ef678cc2
saite <saitejasai57@gmail.com>
2023-12-24 13:22:58 +0530

    new

--- 23.tranposefile.sh
+++ 23.tranposefile.sh
@@ -10,20 +10,6 @@ col2=$(awk -F ' '  '{print $2F}' transpose.txt)
 echo $col1
 echo $col2
 
-
-echo
-echo
-
-for line in $col1
-do
-    echo -n "$line  " 
-done
-echo
-for line in $col2
-do
-    echo -n "$line  " 
-done
-
 #appraoch2
 # cat $file | wc -l # gives no of rows
 # cat $file | head -n 1 | awk '{print NF}' # gives no of columns
@@ -57,6 +43,6 @@ for ((i=0; i<$NoOfCols; i++))
 do
     colNumber=$(($i + 1))
     colValues=$(awk -v col="$colNumber" '{print $col}' $file)
-    IFs='   ' read -r -a inArr <<< "$colValues"
+    IFs=' ' read -r -a inArr <<< "$colValues"
     echo "inArr: ${inArr[@]}"
 done
