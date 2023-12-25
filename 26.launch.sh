5c42b59107c69df97ea45afeb59e25ad3741a99a
saite <saitejasai57@gmail.com>
2023-12-25 23:02:32 +0530

    new

--- 26.launch.sh
+++ 26.launch.sh
@@ -14,7 +14,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    RESULT=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID) --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$svc}]"
+    RESULT=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$svc}]") 
 
     if [ $svc == "web" ]; then
         echo "web Result is: $RESULT"
