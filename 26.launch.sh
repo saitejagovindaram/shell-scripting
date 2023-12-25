4550f680ce3c97186cccd5da4689b5aaa5a583b0
saite <saitejasai57@gmail.com>
2023-12-25 22:13:27 +0530

    new

--- 26.launch.sh
+++ 26.launch.sh
@@ -8,7 +8,7 @@ SG_ID=sg-009b25c0aa06c3c50
 
 for svc in ${SERVICES[@]}
 do
-    if [ $svc == "shipping"]; then
+    if [ $svc == "shipping" ]; then
         INSTANCE_TYPE="t2.small"
     else
         INSTANCE_TYPE="t2.micro"
