9e2905609875f2334564209dc96200cc3c6c5941
saite <saitejasai57@gmail.com>
2023-12-25 22:12:32 +0530

    new

--- 26.launch.sh
+++ 26.launch.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 SERVICES=("web" "catalogue" "mongoDB" "shipping")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SG_ID=sg-009b25c0aa06c3c50
+
 
 for svc in ${SERVICES[@]}
 do
-    echo $svc
+    if [ $svc == "shipping"]; then
+        INSTANCE_TYPE="t2.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID
 done
+
+
