960b309e477d64bc9c9e319b38798b8a96e41081
saite <saitejasai57@gmail.com>
2023-12-27 23:34:34 +0530

    new

--- 26.launch.sh
+++ 26.launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVICES=("web" "catalogue" "mongoDB" "shipping")
+SERVICES=("web" "catalogue" "mongoDB" "shipping" "mysql" "user" "cart" "rabbitmq" "payments" "ratings" "redis"  )
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SG_ID=sg-009b25c0aa06c3c50
@@ -8,7 +8,7 @@ SG_ID=sg-009b25c0aa06c3c50
 
 for svc in ${SERVICES[@]}
 do
-    if [ $svc == "shipping" ]; then
+    if [ $svc == "shipping" ] || [ $svc == "mysql"]; then
         INSTANCE_TYPE="t2.small"
     else
         INSTANCE_TYPE="t2.micro"
@@ -16,14 +16,57 @@ do
 
     RESULT=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$svc}]") 
 
+    Instance_id=$(echo "$RESULT" | jq -r '.Instances[0].InstanceId')
     if [ $svc == "web" ]; then
-        # aws route53 change-resource-record-sets --hosted-zone-id "Z0126075383L1GOMB15Q6" --change-batch "{"Changes":[{"Action":"CREATE","ResourceRecordSet":{"Type":"A","TTL":1,"ResourceRecords":[{"Value":"$RESULT.PublicIpAddress"}]}}]}"
+        PUBLIC_IP=$(aws ec2 describe-instances --instance-ids i-064f57e1e40dbfdef --query "Reservations[0].Instances[0].PublicIpAddress")
         
-        Instance_id=$(echo "$RESULT" | jq -r '.Instances[0].InstanceId')
         # aws ec2 describe-instances --instance-ids $Instance_id
         # use --query to get the public ip
-        echo "$PUBLIC_IP"
+        echo "PUBLICIP: $PUBLIC_IP"
+        aws route53 change-resource-record-sets 
+            --hosted-zone-id "Z0126075383L1GOMB15Q6" 
+            --change-batch "
+                {
+                    "Changes":[
+                        {
+                            "Action":"CREATE",
+                            "ResourceRecordSet":{
+                                "Type":"A",
+                                "TTL":1,
+                                "ResourceRecords":[
+                                    {
+                                        "Value":"$PUBLIC_IP"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            "
 
+    else
+        PRIVATE_IP=$(aws ec2 describe-instances --instance-ids i-064f57e1e40dbfdef --query "Reservations[0].Instances[0].PrivateIpAddress")
+        echo "PRIVATEIP: $PRIVATE_IP"
+        aws route53 change-resource-record-sets 
+            --hosted-zone-id "Z0126075383L1GOMB15Q6" 
+            --change-batch "
+                {
+                    "Changes":[
+                        {
+                            "Action":"CREATE",
+                            "ResourceRecordSet":{
+                                "Type":"A",
+                                "TTL":1,
+                                "ResourceRecords":[
+                                    {
+                                        "Value":"$PRIVATE_IP"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            "
     fi
 done
 
