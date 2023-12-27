4c07b2b18132d7a68e98363bc42c909b5e665d2a
saite <saitejasai57@gmail.com>
2023-12-27 23:39:29 +0530

    new

--- 26.launch.sh
+++ 26.launch.sh
@@ -4,6 +4,8 @@ SERVICES=("web" "catalogue" "mongoDB" "shipping" "mysql" "user" "cart" "rabbitmq
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SG_ID=sg-009b25c0aa06c3c50
+DOMAIN_NAME=saitejag.site
+ZONE_ID=Z0126075383L1GOMB15Q6
 
 
 for svc in ${SERVICES[@]}
@@ -23,50 +25,46 @@ do
         # aws ec2 describe-instances --instance-ids $Instance_id
         # use --query to get the public ip
         echo "PUBLICIP: $PUBLIC_IP"
-        aws route53 change-resource-record-sets 
-            --hosted-zone-id "Z0126075383L1GOMB15Q6" 
-            --change-batch "
-                {
-                    "Changes":[
-                        {
-                            "Action":"CREATE",
-                            "ResourceRecordSet":{
-                                "Type":"A",
-                                "TTL":1,
-                                "ResourceRecords":[
-                                    {
-                                        "Value":"$PUBLIC_IP"
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            "
+        
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Creating a record set for cognito endpoint"
+            ,"Changes": [{
+            "Action"              : "UPSERT"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$svc'.'$DOMAIN_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'$PUBLIC_IP'"
+                }]
+            }
+            }]
+        }'
 
     else
         PRIVATE_IP=$(aws ec2 describe-instances --instance-ids i-064f57e1e40dbfdef --query "Reservations[0].Instances[0].PrivateIpAddress")
         echo "PRIVATEIP: $PRIVATE_IP"
-        aws route53 change-resource-record-sets 
-            --hosted-zone-id "Z0126075383L1GOMB15Q6" 
-            --change-batch "
-                {
-                    "Changes":[
-                        {
-                            "Action":"CREATE",
-                            "ResourceRecordSet":{
-                                "Type":"A",
-                                "TTL":1,
-                                "ResourceRecords":[
-                                    {
-                                        "Value":"$PRIVATE_IP"
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            "
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Creating a record set for cognito endpoint"
+            ,"Changes": [{
+            "Action"              : "UPSERT"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$svc'.'$DOMAIN_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'$PRIVATE_IP'"
+                }]
+            }
+            }]
+        }'
     fi
 done
 
